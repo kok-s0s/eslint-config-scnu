33617730dbbf28bea7072f1ad096cca5f07609aa
kok-s0s <2694308562@qq.com>
2022-04-11 15:24:52 +0800

    重新开始

--- index.js
+++ index.js
@@ -7,16 +7,8 @@ module.exports = {
 		es2021: true,
 		node: true,
 	},
-	extends: ['plugin:react/recommended', 'airbnb'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react', '@typescript-eslint'],
+	extends: [],
+	plugins: [],
 	rules: {
 		'linebreak-style': 0,
 	},
