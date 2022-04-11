c99dc502cb8e277c3eaa68b77b4af703e1ffd183
kok-s0s <2694308562@qq.com>
2022-04-11 15:10:14 +0800

    tempVersion 1.2.18

--- index.js
+++ index.js
@@ -4,62 +4,21 @@ module.exports = {
 	},
 	env: {
 		browser: true,
-		commonjs: true,
 		es2021: true,
 		node: true,
-		jest: true,
 	},
-	extends: [
-		'plugin:react/recommended',
-		'airbnb',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-		'prettier',
-		'prettier/react',
-		'plugin:import/typescript',
-	],
+	extends: ['plugin:react/recommended', 'airbnb'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
 		},
-		ecmaVersion: '12',
+		ecmaVersion: 'latest',
 		sourceType: 'module',
-		project: './tsconfig.json',
 	},
-	plugins: ['react', 'prettier', '@typescript-eslint', 'react-hooks'],
+	plugins: ['react', '@typescript-eslint'],
 	rules: {
 		'linebreak-style': 0,
-		'no-use-before-define': 'warn',
-		'@typescript-eslint/no-use-before-define': ['error'],
-		'react/jsx-filename-extension': [
-			'warn',
-			{
-				extensions: ['.tsx'],
-			},
-		],
-		// 忽略导入规则，以实际项目优先
-		'import/extensions': 'off',
-		'no-shadow': 'off',
-		'@typescript-eslint/no-shadow': ['error'],
-		'@typescript-eslint/explicit-function-return-type': [
-			'error',
-			{
-				allowExpressions: true,
-			},
-		],
-		'max-len': [
-			'warn',
-			{
-				code: 80,
-			},
-		],
-		'react-hooks/rules-of-hooks': 'error',
-		'react-hooks/exhaustive-deps': 'warn',
-		'react/jsx-uses-react': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'import/prefer-default-export': 'off',
-		'react/prop-types': 'off',
 	},
 	settings: {},
 }
